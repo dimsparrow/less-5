b3dd0811bae40a1b78937eae966df4c619d49889
Dimitry Sparrow <info@sparrow.96.lt>
2016-08-19 23:50:25 +0300

    11

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,43 @@
 requirejs.config({
-    baseUrl: './js',
+	baseUrl: './js',
 });
 
 requirejs([
 	'domReady!',
-    'jquery',
-    'slick',
-    'jquery-ui'
+	'jquery',
+	'slick',
+	'jquery-ui'
 ], function (a, $) {
-    'use strict';
+	'use strict';
 
-    $('.nav').slick({
-    	dots: true,
-    	appendDots: '.pagination',
-    	prevArrow: '.prev-arrow',
-    	nextArrow: '.next-arrow'
-    });
-      $( function() {
-    $( "#datepicker" ).datepicker();
+	$('.nav').slick({
+		dots: true,
+		appendDots: '.pagination',
+		prevArrow: '.prev-arrow',
+		nextArrow: '.next-arrow'
+	});
+	  $( function() {
+	$( "#datepicker" ).datepicker();
   } );
+});
+
+var Homepage = {
+	init: function(){
+		this.click();
+	},
+	click: function(){
+		$('.book').on('click', function(){
+			if(!$(this).hasClass('active')){
+				$(this).addClass('active');
+				$(this).next().slideDown('fast');
+			} else {
+				$(this).removeClass('active');
+				$(this).next().slideUp('fast');
+			}
+		});
+	}
+};
+
+$(window).on('load', function(){
+	Homepage.init();
 });
